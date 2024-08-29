c2764f9183f90e50b7b614c815fade23b24a4cc7
SanidhyaIT <19sanidhyarawat@gmail.com>
2024-08-29 13:59:59 +0530

    new change

--- server.js
+++ server.js
@@ -5,6 +5,17 @@ app.get('/api/get', (req,res)=> {
     res.send({message: "hello back to nodejs"})
 })
 
+app.get('/api/get_user_details', (req,res)=> {
+    res.send({
+        user:{
+            name: "Sanidhya",
+            age:22,
+        contact:123456789
+
+        }
+    })
+});
+
 
 
 app.listen(8000, ()=> {
